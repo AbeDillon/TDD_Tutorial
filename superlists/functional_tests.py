6eeeb49fabdab59b013a4c67cbf73d81834bf40a
AbeDillon <adillon@q2ebanking.com>
2013-08-06 15:12:19 -0500

    Reformatted functional_tests.py to use the unittest module

--- superlists/functional_tests.py
+++ superlists/functional_tests.py
@@ -1,32 +1,45 @@
 """Tests that verify the functionality of the program from the end user's perspective."""
 __author__ = 'ADillon'
 
+import unittest
 from selenium import webdriver
 
-browser = webdriver.Firefox()
 
-# Edith has heard about a cool new online to-do app. She goes to check out its homepage
-browser.get("http://localhost:8000")
+class NewVisitorTest(unittest.TestCase):
 
-# She notices the title and header mention to-do lists
-assert "To-Do" in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
 
-# She is invited to enter a to-do item straight away
+    def tearDown(self):
+        self.browser.quit()
 
-# She types "Buy milk" into a text box
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        # Edith has heard about a cool new online to-do app. She goes to check out its homepage
+        self.browser.get("http://localhost:8000")
 
-# When she hits 'Enter', the page updates, and now the page lists "1: Buy milk"
+        # She notices the title and header mention to-do lists
+        self.assertIn("To-Do", self.browser.title)
+        self.fail("Finish the test!")
 
-# There is still a text box inviting her to add another item.
+        # She is invited to enter a to-do item straight away
 
-# She enters "Buy eggs" and presses 'Enter' to see "2: Buy eggs" added to her list
+        # She types "Buy milk" into a text box
 
-# Edith sees that the site has generated a URL for her to revisit her to-do list in the future
+        # When she hits 'Enter', the page updates, and now the page lists "1: Buy milk"
 
-# There is some explanatory text regarding the URL
+        # There is still a text box inviting her to add another item.
 
-# She visits the URL to see that her to-do list is still there
+        # She enters "Buy eggs" and presses 'Enter' to see "2: Buy eggs" added to her list
 
-# Satisfied, Edith goes back to sleep
+        # Edith sees that the site has generated a URL for her to revisit her to-do list in the future
 
-browser.quit()
+        # There is some explanatory text regarding the URL
+
+        # She visits the URL to see that her to-do list is still there
+
+        # Satisfied, Edith goes back to sleep
+
+
+if __name__ == "__main__":
+    unittest.main()
